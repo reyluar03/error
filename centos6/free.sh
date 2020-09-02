c67402b0bcc97511cf70f109b1da68ae4d600e7d
reyluar03 <47968779+reyluar03@users.noreply.github.com>
2020-09-02 10:38:47 +0800

    Update free.sh

--- centos6/free.sh
+++ centos6/free.sh
@@ -106,7 +106,7 @@ wget -O /etc/init.d/dropbear "https://raw.githubusercontent.com/reyluar03/error/
 
 #install PHP
 yum install gcc php-devel php-pear libssh2 libssh2-devel make -y
-pecl install -f ssh2 -y
+yes | pecl install -f ssh2 -y
  echo extension=ssh2.so > /etc/php.d/ssh2.ini
 service httpd restart
  php -m | grep ssh2
